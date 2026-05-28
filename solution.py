40450db9afee4d2f24d8ce0187ffc5d552389a19
Dmitrii <yakunin.mitya13@yandex.ru>
2026-05-28 14:36:46 +0300

    solution update

--- solution.py
+++ solution.py
@@ -9,13 +9,12 @@ model = YOLO('yolo11n.pt')
 result = model('run_01/photo/frame1.jpg')
 result = result[0]
 
-FOV_x = 65 #!!! РАЗОБРАТЬСЯ
-FOV_y = 40 #!!! РАЗОБРАТЬСЯ
-
-fx = W / 2 * math.tan(math.radians(FOV_x / 2))
-fy = H / 2 * math.tan(math.radians(FOV_y / 2))
-
 boxes = result.boxes
+
+x1 = 0
+y1 = 0
+x2 = 0
+y2 = 0
 
 for i in range(len(boxes)):
         class_id = int(boxes.cls[i].tolist())
@@ -25,4 +24,31 @@ for i in range(len(boxes)):
         x1, y1, x2, y2 = map(int, boxes.xyxy[i].tolist())
         print(f"  Рамка: [{x1}, {y1}, {x2}, {y2}]")
 
-        alpha = math.arctan()
+FOV_x = 65 #!!! РАЗОБРАТЬСЯ
+FOV_y = 40 #!!! РАЗОБРАТЬСЯ
+
+fx = W / (2 * math.tan(math.radians(FOV_x / 2)))
+fy = H / (2 * math.tan(math.radians(FOV_y / 2)))
+cx = W / 2
+x_center = (x1 + x2) / 2
+alpha_rad = math.atan2(x_center - cx,  fx)
+
+print(alpha_rad)
+print()
+
+import json
+
+def load_lidar(run_path='run_01'):
+    points = []
+    dist = []
+    with open(f'{run_path}/lidar/lidar_scans.jsonl', 'r') as f:
+        for line in f:
+            data = json.loads(line)
+            for i in range(len(data['angles_rad'])):
+                angle = data['angles_rad'][i]
+                if abs(angle - alpha_rad) < 0.01:
+                     points.append(angle)
+                     dist.append(data['ranges_m'][i]) # TODO: проверить что не нужно прибавлять 1 из-за null (len должен совпадать у списков)
+    return dist
+
+print(load_lidar())
